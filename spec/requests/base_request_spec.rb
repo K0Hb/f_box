0c03206ceabc9758a4ec2a28a7f17567fc70f94c
Vjacheslav <goplit2010.konovalov@yandex.ru>
2022-10-24 19:37:12 +0300

    add tests check domains in respone

--- spec/requests/base_request_spec.rb
+++ spec/requests/base_request_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Base', type: :request do
   after(:each) do
     redis = Redis::Namespace.new('f_box', redis: Redis.new)
-    redis.del(Time.now.to_i)
+    redis.del('links')
   end
 
   describe 'POST /visited_links' do
@@ -101,7 +101,21 @@ RSpec.describe 'Base', type: :request do
 
         expect(response.status).to eq(200)
         expect(JSON.parse(response.body)['status']).to eq('OK')
-        expect(JSON.parse(response.body)['domains']).to eq(%w[test_url3 test_url1])
+        expect(JSON.parse(response.body)['domains']).to eq(%w[test_url1 test_url3])
+      end
+    end
+
+    context 'when two urls with the same domain' do
+      it 'return correct domain' do
+        post '/api/v1/visited_links',
+          params: { links: %w[https://test_url1.ru https://test_url1.ru/user] }.to_json,
+          headers: headers
+
+        get '/api/v1/visited_domains', params: get_params
+
+        expect(response.status).to eq(200)
+        expect(JSON.parse(response.body)['status']).to eq('OK')
+        expect(JSON.parse(response.body)['domains']).to eq(['test_url1.ru'])
       end
     end
 
